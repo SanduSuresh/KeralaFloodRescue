ce4ad0edc5996ca4a1ef27ca176b72aa896b3295
SanduSuresh <sandeepsureshbacks@gmail.com>
2020-03-02 00:17:23 +0530

    UI Updations

--- app/src/main/java/com/example/keralafloodrescue/VolunteerLogin.java
+++ app/src/main/java/com/example/keralafloodrescue/VolunteerLogin.java
@@ -8,10 +8,18 @@ import android.view.View;
 import android.widget.Button;
 
 public class VolunteerLogin extends AppCompatActivity {
+    Button signupbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer_login);
-
+        signupbtn = (Button) findViewById(R.id.signupBtn);
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),VolunteerSignup.class);
+                startActivity(i);
+            }
+        });
     }
 }
