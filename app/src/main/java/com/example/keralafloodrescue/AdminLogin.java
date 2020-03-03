871df61f198baf76480d4d33c528af12b17b39d0
SanduSuresh <sandeepsureshbacks@gmail.com>
2020-03-03 10:55:53 +0530

    Admin Login minor fix

--- app/src/main/java/com/example/keralafloodrescue/AdminLogin.java
+++ app/src/main/java/com/example/keralafloodrescue/AdminLogin.java
@@ -9,9 +9,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class AdminLogin extends AppCompatActivity {
     Button loginbtn;
     EditText email,pass;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("admins");
+
+    String name;
+    String password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +32,30 @@ public class AdminLogin extends AppCompatActivity {
         loginbtn = (Button) findViewById(R.id.loginBtn);
         email = (EditText) findViewById(R.id.emailText);
         pass = (EditText) findViewById(R.id.passwordText);
+
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(email.getText().toString().equals("admin")&&pass.getText().toString().equals("admin")) {
+
+                //myRef.child("admin1").child("name").setValue("unni");
+                //myRef.child("admin1").child("pass").setValue("123");
+
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        name = (String) dataSnapshot.child("admin1").child("name").getValue();
+                        password = (String) dataSnapshot.child("admin1").child("pass").getValue();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+
+                    }
+                });
+                if(email.getText().toString().equals(name)&&pass.getText().toString().equals(password)) {
                     Intent i = new Intent(getApplicationContext(), AdminHome.class);
                     startActivity(i);
                 }
